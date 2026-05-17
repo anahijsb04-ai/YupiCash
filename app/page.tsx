57823c65b82f01877f0ca58750a80f7ada4c2581
anahijsb04-ai <anahijsb04@gmail.com>
2026-05-17 11:27:40 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -207,7 +207,7 @@ export default function Home() {
             </div>
 
             <a
-              href="/app-release.apk"
+              href="https://collection-yupicash.vercel.app"
               download
               className="
                 relative
